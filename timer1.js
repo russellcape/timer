9e133e8a945887fe78cc11cdfc0a9e857e0bdf7e
Russell Cape <russellcape98@gmail.com>
2019-10-24 19:00:58 +0000

    Timer function reads input in terminal, checks to see if it satisfies any conditions, if not, it beeps after input time has passed

--- timer1.js
+++ timer1.js
@@ -1,1 +1,12 @@
-process.stdout.write('\x07');
+ 
+const timer = function() {
+	const timers = process.argv.slice(2);
+  for (let time = 0; time < timers.length; time++) {
+  if(timers[time] < 0 || undefined || isNaN(timers[time])) {
+  	return;
+  } 
+  setTimeout(()=> {process.stdout.write('\x07')},timers[time]*1000);
+	}
+};
+
+ timer();
